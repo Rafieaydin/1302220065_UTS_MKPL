93e6ec4efaca7bcb508c1811e11a428b3e3e6340
Rafieaydin <rafie.aydin1@gmail.com>
2025-04-17 21:58:53 +0700

    remove all comment

--- src/lib/FamilyDetails.java
+++ src/lib/FamilyDetails.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lib;
 
 import java.util.LinkedList;
 import java.util.List;
-
-/**
- *
- * @author LOQ
- */
 public class FamilyDetails {
     private String spouseName;
     private String spouseIdNumber;
