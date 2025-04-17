1ecfd46ef0f2851035a7d3a4a0082a764985162c
Rafieaydin <rafie.aydin1@gmail.com>
2025-04-17 20:56:47 +0700

    hapus comment di table employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -18,13 +18,7 @@ public class Employee {
                 this.family = family;
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
 	public int getAnnualIncomeTax() {
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		return TaxFunction.calculateTax(salary.getMonthlySalary(), salary.getOtherMonthlyIncome(), JoinDate.monthWorkingInYear(), salary.getAnnualDeductible(), family.getSpouseIdNumber().equals(""), family.getNumberOfChildren());
 	}
 }
