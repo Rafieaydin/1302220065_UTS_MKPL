403f9515b1630aa208b7397b8c17e122fbb5bd14
Rafieaydin <rafie.aydin1@gmail.com>
2025-04-17 21:22:30 +0700

    change if statement to switch case statement

--- src/lib/Salary.java
+++ src/lib/Salary.java
@@ -14,27 +14,31 @@ public class Salary {
     private int otherMonthlyIncome;
     private int annualDeductible;
     private boolean isForeigner;
+    
     public Salary(boolean isForeigner){
         this.isForeigner = isForeigner;
     }
     
     public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
+             switch (grade) {
+                case 1:
+                      monthlySalary = 5000000;
+                       if (isForeigner) {
+                           monthlySalary = (int) (3000000 * 1.5);
+                       }
+                case 2:
+                    monthlySalary = 5000000;
+                    if (isForeigner) {
+                        monthlySalary = (int) (3000000 * 1.5);
+                    }
+                case 3:
+                    monthlySalary = 7000000;
+                    if (isForeigner) {
+                        monthlySalary = (int) (3000000 * 1.5);
+                    }
+                default:
+                    throw new IllegalArgumentException("Invalid grade: " + grade);
+            }
 	}
 	
 	public void setAnnualDeductible(int deductible) {	
